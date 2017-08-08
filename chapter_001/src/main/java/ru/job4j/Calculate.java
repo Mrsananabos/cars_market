3138f10c7bc081909f0cc9d523a9f01195a0cb50
Mrsananabos <anlala@bk.ru>
2017-08-08 10:55:34 +0500

    task #4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j;
 
+/**
+ *Calculate.
+ *
+ * @author Anna Shveytser (mailto:anlala@bk.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
-   public static void main(String[] args){
-      System.out.println("Hello, world!");}
-
+   /**
+    * Main.
+    * @param args - args.
+    */
+    public static void main(String[] args){
+        System.out.println("Hello, world!");
+   }
 }
