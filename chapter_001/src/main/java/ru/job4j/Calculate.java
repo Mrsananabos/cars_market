31cad64792f5c3cf1abc545b34a69dba7cf91390
Mrsananabos <anlala@bk.ru>
2017-08-22 23:33:01 +0500

    2.3. Элементарный калькулятор.[#22627]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -4,10 +4,24 @@ public class Calculate {
     private double result;
 
     public void add(double first, double second) {
+
         this.result = first + second;
     }
+    public void subtrack(double first, double second) {
 
+        this.result = first - second;
+    }
+    public void div(double first, double second) {
+
+        this.result = first/second;
+    }
+    public void  multiple (double first, double second) {
+
+        this.result = first*second;
+    }
     public double getResult() {
+
         return this.result;
     }
+
 }
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -15,4 +15,28 @@ public class CalculateTest {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+    @Test
+     public void whenSubtractOneMinusOneThenZero() {
+        Calculate calc = new Calculate();
+        calc.subtrack(1D, 1D);
+        double result = calc.getResult();
+        double expected = 0;
+        assertThat(result, is(expected));
+    }
+    @Test
+public void whenDivOneDivideOneThenOne() {
+        Calculate calc = new Calculate();
+        calc.div(1D, 1D);
+        double result = calc.getResult();
+        double expected = 1;
+        assertThat(result, is(expected));
+    }
+    @Test
+public void whenMultipleOneMultiplyOneThenOne() {
+        Calculate calc = new Calculate();
+        calc.multiple(1D, 1D);
+        double result = calc.getResult();
+        double expected = 1;
+        assertThat(result, is(expected));
+    }
 }
