3b30ea57e76ff8dbc28ecde4e7386f57730caa9e
Mrsananabos <anlala@bk.ru>
2017-10-14 14:15:44 +0300

    tracker

---
+++ chapter_002/src/main/java/ru/job4j/start/Item.java
@@ -0,0 +1,46 @@
+package ru.job4j.start;
+
+
+public class Item {
+   
+   private String id;
+   public String name;
+   public String desc;
+   public long created;
+   public String comments;
+
+	public Item (String name, String desc, long created, String comments) {
+		this.name = name;
+		this.desc=desc;
+		this.created=created;
+		this.comments=comments;
+		}
+	
+	public String getId() {
+		return this.id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+
+
+	public String getName() {
+		return this.name;
+		}
+
+	public String getDesc() {
+		return this.desc;
+		}
+
+	public long getCreated() {
+		return this.created;
+		}
+
+	public String getComment() {
+		return this.comments;
+		}
+}
+	
+   
