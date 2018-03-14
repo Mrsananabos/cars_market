ac4cc7e1097ded3bde9f098109d979d76f097fb9
Mrsananabos <anlala@bk.ru>
2018-03-14 12:07:53 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#9539]

--- chapter_004/src/main/java/ru/job4j/Collections/MatrixIterator.java
+++ chapter_004/src/main/java/ru/job4j/Collections/MatrixIterator.java
@@ -1,33 +1,41 @@
 package ru.job4j.Collections;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class MatrixIterator implements Iterator{
 
     public int[][] array;
     int currentIndex = 0;
     int currentColumn=0;
-    int column=array.length;
-    int index=array[0].length;
+    int column;
+    int index;
 
 
-    public MatrixIterator(int[][] array){
+    public MatrixIterator(int[][] array) throws NoSuchElementException{
         this.array=array;
+        if (array.length == 0) throw new NoSuchElementException("нет значений");
+        this.column=array.length;
+        this.index=array[0].length;
+
     }
 
 
     @Override
     public boolean hasNext() {
-        return (currentIndex<index && currentColumn<column);
+        return (currentIndex<index) ||
+                (currentIndex==index && currentColumn<(column-1));
     }
 
     @Override
-    public Object next() {
-        if (currentIndex>index){
+    public Object next() throws NoSuchElementException {
+        if (currentIndex>=index){
             currentIndex=0;
             currentColumn++;
-            index=array[column].length;
+            if (currentColumn==column) throw new NoSuchElementException("нет значений");
+            this.index=array[currentColumn].length;
         }
+
         return array[currentColumn][currentIndex++] ;
     }
 
