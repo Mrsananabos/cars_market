3db916056f5e5fb9e8c3311f88b52a93b77cf320
Mrsananabos <anlala@bk.ru>
2018-04-10 18:34:07 +0300

    1. Реализовать коллекцию Set на массиве [#996]

---
+++ chapter_004/src/main/java/ru/job4j/Collections/Set/SimpleSet.java
@@ -0,0 +1,22 @@
+package ru.job4j.Collections.Set;
+
+import ru.job4j.Collections.List.DynamicContainer;
+
+public class SimpleSet<T> extends DynamicContainer<T>{
+
+        public SimpleSet(int size) {
+            super(size);
+        }
+
+        public void add(T value) {
+            for (int i = 0; i<super.index;i++){
+                if (value.equals(super.container[i])) {
+                    return;
+                }
+            }
+            super.add(value);
+        }
+
+    }
+
+
