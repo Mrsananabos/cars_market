dc4977371634f1825dbecec13154a04c9d51915a
Mrsananabos <anlala@bk.ru>
2018-04-15 15:08:07 +0300

    3. Переопределить только hashCode [#1003][#49653]

--- chapter_004/src/main/java/ru/job4j/Collections/Map/User.java
+++ chapter_004/src/main/java/ru/job4j/Collections/Map/User.java
@@ -8,14 +8,19 @@ public class User {
 
     private String name;
     private int children;
+    private int dayOfBirth;
+    private int mounthOfBirth;
+    private int yearOfBirth;
     private Calendar birthday = Calendar.getInstance();
 
     public User(String name, int children, int dayBirthday, int mounthBirthday, int yearBirthday) {
         this.name = name;
         this.children = children;
         this.birthday.set(yearBirthday, mounthBirthday, dayBirthday);
+        this.dayOfBirth=dayBirthday;
+        this.mounthOfBirth=mounthBirthday;
+        this.yearOfBirth= yearBirthday;
     }
-
 
 
     public String getName() {
@@ -30,16 +35,15 @@ public class User {
         return birthday;
     }
 
+
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        User user = (User) o;
-
-        if (children != user.children) return false;
-        if (name != null ? !name.equals(user.name) : user.name != null) return false;
-        return birthday != null ? birthday.equals(user.birthday) : user.birthday == null;
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 37 * result + children;
+        result = 37 * result + dayOfBirth;
+        result = 37 * result + mounthOfBirth;
+        result = 37 * result + yearOfBirth;
+        return result;
     }
 
 
@@ -47,10 +51,10 @@ public class User {
     public static void main(String[] args) {
         Map<User, Object> map = new HashMap<>();
         User u1 = new User("Sasha", 2, 13, 7, 1980);
-       User u2 = new User("Sasha", 2, 13, 7, 1980);
+        User u2 = new User("Sasha", 2, 13, 7, 1980);
         map.put(u1, new Object());
         map.put(u2, new Object());
         System.out.println(map);
-
     }
+
 }
