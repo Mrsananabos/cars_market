891a4d812ef449b7dc6accbc378e273ddffbdb2a
Mrsananabos <anlala@bk.ru>
2018-05-20 20:55:47 +0300

    1. Проиллюстрировать проблемы с многопоточностью. [#1096]

---
+++ chapter_005/src/main/java/ru/job4j/threads/ThreadingProblem.java
@@ -0,0 +1,34 @@
+package ru.job4j.threads;
+
+public class ThreadingProblem {
+    private int count = 0;
+
+    public void doCounter() {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000000; i++) {
+                    count++;
+                }
+                System.out.println(count);
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000000; i++) {
+                    count++;
+                }
+                System.out.println(count);
+            }
+        });
+
+        thread1.start();;
+        thread2.start();
+    }
+
+    public static void main(String[] args) {
+        new ThreadingProblem().doCounter();
+    }
+}
