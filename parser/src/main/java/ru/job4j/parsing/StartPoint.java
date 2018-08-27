455cd613bccc416a005eb93c24eccbd7fd227e8c
Mrsananabos <anlala@bk.ru>
2018-08-27 13:54:01 +0300

    Парсер вакансий на sql.ru [#1731]

---
+++ parser/src/main/java/ru/job4j/parsing/StartPoint.java
@@ -0,0 +1,22 @@
+package ru.job4j.parsing;
+
+public class StartPoint {
+    private int fromElementNumber;
+    private int fromPageNumber;
+
+    public int getFromElementNumber() {
+        return fromElementNumber;
+    }
+
+    public void setFromElementNumber(int fromElementNumber) {
+        this.fromElementNumber = fromElementNumber;
+    }
+
+    public int getFromPageNumber() {
+        return fromPageNumber;
+    }
+
+    public void setFromPageNumber(int fromPageNumber) {
+        this.fromPageNumber = fromPageNumber;
+    }
+}
